afa0efe1047449129cb9aa06ae2123100205dd42
Peter Chiu <pchiu@glgroup.com>
2019-01-22 09:06:58 +0800

    unicode-friendly slugify to support chinese channel names

--- packages/rocketchat-lib/lib/getValidRoomName.js
+++ packages/rocketchat-lib/lib/getValidRoomName.js
@@ -1,5 +1,20 @@
 import { Meteor } from 'meteor/meteor';
-import s from 'underscore.string';
+
+function hash(str) {
+	// https://github.com/darkskyapp/string-hash/blob/master/index.js
+
+	let hash = 5381;
+	let i = str.length;
+
+	while (i) {
+		hash = (hash * 33) ^ str.charCodeAt(--i);
+	}
+
+	/* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
+   * integers. Since we want the results to be always positive, convert the
+   * signed int to an unsigned by doing an unsigned bitshift. */
+	return (hash >>> 0).toString();
+}
 
 RocketChat.getValidRoomName = function getValidRoomName(displayName, rid = '') {
 	let slugifiedName = displayName;
@@ -13,7 +28,7 @@ RocketChat.getValidRoomName = function getValidRoomName(displayName, rid = '') {
 				throw new Meteor.Error('error-duplicate-channel-name', `A channel with name '${ displayName }' exists`, { function: 'RocketChat.getValidRoomName', channel_name: displayName });
 			}
 		}
-		slugifiedName = s.slugify(displayName);
+		slugifiedName = hash(displayName);
 	}
 
 	let nameValidation;
